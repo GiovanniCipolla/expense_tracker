b037119fb10d971df1ee1bbdc42c9654890b115b
Giovanni Cipolla <yayacipolla@icloud.com>
2023-07-21 10:09:06 +0200

    aggiornamenti

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exspense_tracker/theme_settings.dart';
 import 'package:exspense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
@@ -8,67 +9,14 @@ var kDarkColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 56, 70));
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then(
-  //   (fn) =>
+  WidgetsFlutterBinding.ensureInitialized();      
+ 
   runApp(
       MaterialApp(
-        darkTheme: ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: kDarkColorScheme,
-          brightness: Brightness.dark,
-          appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kDarkColorScheme.onPrimaryContainer,
-              foregroundColor: kDarkColorScheme.primaryContainer),
-          cardTheme: const CardTheme().copyWith(
-              color: kDarkColorScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kDarkColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: kDarkColorScheme.onSecondaryContainer,
-                    fontSize: 17),
-              ),
-        ),
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer),
-          cardTheme: const CardTheme().copyWith(
-              color: kColorScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: kColorScheme.onSecondaryContainer,
-                    fontSize: 17),
-              ),
-        ),
-        themeMode: ThemeMode.system,
+        darkTheme:getDarkThemeData(),
+        theme: getLightThemeData(),
+        themeMode: ThemeMode.system, // dark o light
         home: const Expenses(),
       ),
     );
-  // );
 }
-
-
-// bloccare l'all solo in verticale , no orizzontale
-// WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then(
-  //   (fn) =>
